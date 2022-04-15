a790712899dfae779d53019d10796b1aca7fb591
uglyFilinx <ibekwe25@gmail.com>
2022-04-15 16:04:41 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+table File  31 lines (28 sloc)  348 Bytes
+#include "main.h"
+
+/**
+ *print_diagonal - draws a diagonal line on the terminal.
+ *@n: number of lines
+ *Return: returns 0
+ */
+
+void print_diagonal(int n)
+{
+	int a = 0;
+	int b = 0;
+
+	if (n > 0)
+	{
+		while (a < n)
+		{
+			while (b < a)
+			{
+				_putchar(' ');
+				b++;
+			}
+			a++;
+			b = 0;
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
